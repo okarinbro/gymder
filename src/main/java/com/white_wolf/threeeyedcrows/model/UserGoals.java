c3ba92c5738b91b5320ac3eda68548631277a270
meehaw1337-laptop <15misiek12@wp.pl>
2020-02-29 14:32:56 +0100

    Entity annotations added to models, initial database connection properties added

--- src/main/java/com/white_wolf/threeeyedcrows/model/UserGoals.java
+++ src/main/java/com/white_wolf/threeeyedcrows/model/UserGoals.java
@@ -1,16 +1,12 @@
 package com.white_wolf.threeeyedcrows.model;
 
 import lombok.Data;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@EnableAutoConfiguration
 @Data
 @Table(name = "user_goals")
+@Entity
 public class UserGoals {
     private Long weekNumber;
     private boolean completed;
