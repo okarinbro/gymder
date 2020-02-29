b50371297d84ff1529a81e4961fa4ebcd94b7f83
okarinbro <prz.jab98@gmail.com>
2020-02-29 09:54:01 +0100

    Initial commit

---
+++ src/test/java/com/white_wolf/threeeyedcrows/ThreeEyedCrowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.white_wolf.threeeyedcrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeEyedCrowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
