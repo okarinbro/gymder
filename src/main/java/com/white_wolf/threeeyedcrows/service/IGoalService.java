75f2d72ba1f394eaf3e9d12c6a71912ce67567ff
okarinbro <prz.jab98@gmail.com>
2020-03-01 03:04:38 +0100

    Add get request for all possible goals from database

--- src/main/java/com/white_wolf/threeeyedcrows/service/IGoalService.java
+++ src/main/java/com/white_wolf/threeeyedcrows/service/IGoalService.java
@@ -1,6 +1,7 @@
 package com.white_wolf.threeeyedcrows.service;
 
 import com.white_wolf.threeeyedcrows.exception.NotFoundException;
+import com.white_wolf.threeeyedcrows.model.Goal;
 import com.white_wolf.threeeyedcrows.model.UserGoal;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface IGoalService {
     void addUserGoal(UserGoal userGoal);
 
     void updateUserGoal(UserGoal userGoal) throws NotFoundException;
+
+    List<Goal> getAllGoals();
 }
