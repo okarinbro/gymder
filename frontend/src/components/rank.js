e9ee62ab33532f6aacae3ba18b5eb405b1c8ba60
Michał Przybycien <mprzybycien@andea.com>
2020-03-01 06:59:01 +0100

    Add rank api

--- frontend/src/components/rank.js
+++ frontend/src/components/rank.js
@@ -1,20 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/rank.css';
+const axios = require("axios")
 
 class Ranking extends Component {
     state = {
         users: [
-            {id: 1, username: "Michał Przybycień", points: 1486},
-            {id: 2, username: "Michał Maksoń", points: 1456},
-            {id: 3, username: "Michał Komar", points: 1337},
-            {id: 4, username: "Przemysław Jabłecki", points: 1278}]
+            // {id: 1, username: "Michał Przybycień", points: 1486},
+            // {id: 2, username: "Michał Maksoń", points: 1456},
+            // {id: 3, username: "Michał Komar", points: 1337},
+            // {id: 4, username: "Przemysław Jabłecki", points: 1278}
+        ]
+    }
+    componentWillMount() {
+        axios.get("http://localhost:8080/api/users/friends/").then(
+            (response) => this.setState({ users: response.data })
+        ).catch((error) => {
+            console.log(error)
+        })
     }
 
     render() {
         return (<div className="rankMainDiv">
             <ol>
                 {this.state.users.map(user => <li key={user.id}
-                                                  className="rankListItem">{user.username}: {user.points}</li>)}
+                    className="rankListItem">{user.username}: {user.points}</li>)}
             </ol>
         </div>);
     }
